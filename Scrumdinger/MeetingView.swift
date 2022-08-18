7a5e7e478e1af90ce599ef0d73137cbaa913b638
alanahbell10 <abell22@students.towson.edu>
2022-08-18 17:19:35 -0400

    renaming files

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Scrumdinger,
+//
+//  Created by Alanah Bell on 8/17/22.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
