dbcf5e9d7ca474205c9406a4d234b528f0789f07
alanahbell10 <abell22@students.towson.edu>
2022-08-17 20:36:53 -0400

    Initial Commit

---
+++ Scrumdinger,/Scrumdinger_App.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_App.swift
+//  Scrumdinger,
+//
+//  Created by Alanah Bell on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
